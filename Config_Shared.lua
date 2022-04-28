3f682d5cf8983f57c00bf40860f7127f063ffd64
Zydsonek <98161953+Zydson@users.noreply.github.com>
2022-04-28 12:48:37 +0200

    Add files via upload

---
+++ Config_Shared.lua
@@ -0,0 +1,23 @@
+Config = {}
+-- [AVAILABLE LANGUAGES] --
+-- PL, EN, DE
+Config.Translation = "PL" 
+
+-- [FRAMEWORK] --
+Config.FrameWork = {
+    ESX = true,
+    QBcore = false,
+    StandAlone = false,
+}
+-- ESX GetSharedObject event if needed
+Config.ESXSharedEvent = "esx:getShtestaredObjtestect"
+
+Config.Weapons = {
+[453432689] = "Pistolet", -- PISOLET
+[1593441988] = "Pistolet Bojowy", -- BOJOWKA
+[2285322324] = "Pukawka Mk2", -- PUKAWKA MK2
+[3218215474] = "Pukawka", -- PUKAWKA
+[3219281620] = "Pistolet Mk2", -- MK2
+[137902532] = "Pistolet Vintage", -- VINTAGE
+[3523564046] = "Ciezki Pistolet", -- HEAVY
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'bodacious'
+game 'gta5'
+lua54 'yes'
+
+
+server_scripts {
+    "Server/server.lua",
+	"Config_Shared.lua",
+}
+
+client_scripts {
+    "Client/client.lua",
+	"Config_Shared.lua",
+    "Translation/pl.lua",
+    "Translation/en.lua",
+    "Translation/de.lua",
+}
