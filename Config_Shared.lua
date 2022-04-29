144d86a46b62263131abfe78b32a3f153381ea27
Zydsonek <98161953+Zydson@users.noreply.github.com>
2022-04-29 18:25:33 +0200

    Update Config_Shared.lua

--- Config_Shared.lua
+++ Config_Shared.lua
@@ -8,7 +8,6 @@ Config.Translation = "PL"
 Config.FrameWork = {
     ESX = true,
     QBcore = false,
-    StandAlone = false,
 }
 
 -- [JOB NAMES WITH ACCESS] --
@@ -18,13 +17,27 @@ Config.JobName = {
 }
 
 -- [LAB COORDS] --
-Config.LabCoords = vector3(482.513,-988.598,29.68)
+Config.LabCoordsGive = vector3(482.513,-988.598,29.68)
+Config.LabCoordsCloset = vector3(482.7572,-992.8956,29.68)
 
 -- ESX GetSharedObject event if needed -- TODO: automaticate this
 Config.ESXSharedEvent = "esx:getShtestaredObjtestect"
 
 Translation["PL"] = {
     takeshell = "Zapakowałeś łuskę do woreczka, zanieś go do laboratorium",
+	description = "Opis [5-30 znakow]",
+	descriptionerror = "Błąd w opisie",
+	giveshell = "Oddałeś łuske, dokumenty trafią do szafy po zbadaniu jej",
+	noshells = "Nie posiadasz przy sobie żadnych łusek",
+	pressgive = "Naciśnij ~INPUT_CONTEXT~ aby oddać łuske do laboratorium",
+	pressopen = "Naciśnij ~INPUT_CONTEXT~ aby otworzyć szafę",
+	binddesc = "Otwieranie menu w labach",
+	shell = "Łuska",
+	police = "Policyjne",
+	civil = "Cywilne",
+	docs = "Dokumenty",
+	lab = "Laboratorium",
+	delete = "[Usun]",
 	pistol = "Pistolet",
 	combat = "Pistolet Bojowy",
 	snsmk = "Pukawka Mk2",
@@ -36,6 +49,19 @@ Translation["PL"] = {
 
 Translation["EN"] = {
     takeshell = "U packed shell to bag, take it to labs",
+	description = "Description [5-30 chars]",
+	descriptionerror = "Eror in description",
+	giveshell = "You gave the bag, the documents will go to the closet after examining it",
+	noshells = "You dont have any shells in your pocket",
+	pressgive = "Press ~INPUT_CONTEXT~ to return shell to the laboratory",
+	pressopen = "Press ~INPUT_CONTEXT~ to open closet",
+	binddesc = "Open lab menu",
+	shell = "Shell",
+	police = "Police",
+	civil = "Civil",
+	docs = "Documents",
+	lab = "Lab",
+	delete = "[Delete]",
 	pistol = "Pistol",
 	combat = "Combat Pistol",
 	snsmk = "SNS Pistol Mk2",
